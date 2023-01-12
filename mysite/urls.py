9399f025b3e930767c27823fc341b9647c27f6ef
HungNVHaUI <hungnv.haui@gmail.com>
2023-01-12 21:47:10 +0700

    update 12.1.23

--- mysite/urls.py
+++ mysite/urls.py
@@ -14,8 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
+
     path('admin/', admin.site.urls),
+    path('home/', include('home.urls')),
+    path('blog/', include('blog.urls')),
+    path('contact/', include('contact.urls')),
+    path('usermember/', include('usermember.urls')),
+    path('loadfile/', include('loadfile.urls')),
 ]
